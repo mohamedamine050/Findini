a22c1bbfecc9c1650f41317c1c173ddfcfed3c27
mohamed amine abdrrazek <mohamedamine.abdrrazek@supcom.tn>
2025-05-06 19:49:48 +0100

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,1 +1,11 @@
 
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const auth = require("../middleware/authenticate");
+
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.get("/profile", auth, userController.profile);
+
+module.exports = router;
