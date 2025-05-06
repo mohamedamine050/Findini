8b7fda331ef7e30e5141c3cd9953aa84a176de0a
mohamed amine abdrrazek <mohamedamine.abdrrazek@supcom.tn>
2025-05-06 19:51:17 +0100

    Update projects.js

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const projectController = require("../controllers/projectController");
+const auth = require("../middleware/authenticate");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => cb(null, file.originalname),
+});
+const upload = multer({ storage });
+
+router.get("/", projectController.getProjects);
+router.post("/", auth, upload.fields([{ name: "image" }]), projectController.addProject);
+
+module.exports = router;
 
