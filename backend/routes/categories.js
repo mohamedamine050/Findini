0b4cb02d8f7cf9029111e61ad8204171eca5bd34
mohamed amine abdrrazek <mohamedamine.abdrrazek@supcom.tn>
2025-05-06 19:50:39 +0100

    Update categories.js

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const categoryController = require("../controllers/categoryController");
+const auth = require("../middleware/authenticate");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => cb(null, file.originalname),
+});
+const upload = multer({ storage });
+
+router.get("/", categoryController.getAllCategories);
+router.post("/", auth, upload.fields([{ name: "image" }]), categoryController.addCategory);
+router.delete("/:id", auth, categoryController.deleteCategory);
+
+module.exports = router;
 
