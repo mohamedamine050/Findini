d2b1da355989541c73ecbc022322606a1f861e13
mohamed amine abdrrazek <mohamedamine.abdrrazek@supcom.tn>
2025-05-06 19:40:54 +0100

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,1 +1,20 @@
+const multer = require('multer');
+const path = require('path');
+
+// Pour les catégories
+const categoryStorage = multer.diskStorage({
+  destination: "../my-app/public",
+  filename: (req, file, cb) => cb(null, file.originalname)
+});
+
+// Pour les projets
+const projectStorage = multer.diskStorage({
+  destination: "../my-app/public",
+  filename: (req, file, cb) => cb(null, file.originalname)
+});
+
+const uploadCategory = multer({ storage: categoryStorage });
+const uploadProject = multer({ storage: projectStorage });
+
+module.exports = { uploadCategory, uploadProject };
 
